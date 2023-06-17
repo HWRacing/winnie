f7c7336527d68a21445f9708f2ab9364a15d980a
Hamish Knox <hrjkknox@gmail.com>
2023-06-17 13:29:51 +0100

    Implement printByteArrayWithLabel

--- src/winnie/formatting.py
+++ src/winnie/formatting.py
@@ -3,3 +3,7 @@ from typing import List
 def printHexList(text: str, n: List[int]):
 	result = [f"0x{num:02X}" for num in n]
 	print(text + str(result))
+
+def printByteArrayWithLabel(text: str, n: bytearray):
+	intList = list(bytearray)
+	printHexList(text, n)
