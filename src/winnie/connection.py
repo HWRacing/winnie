f5fdceb659953acb7d2ca26e6aaed93de525a1dc
Hamish Knox <hrjkknox@gmail.com>
2023-06-25 16:18:32 +0100

    Overhaul message sending (#10)
    * Implement constructCRO
    
    * Implement sendCRO
    
    * Add counter to CROs
    
    * Implement intToByteArray
    
    * Overhaul connect
    
    * Overhaul disconnect
    
    * Update exchangeID
    
    * Upload getSeed
    
    * Update getCCPVersion
    
    * Update download
    
    * Update downloadSix
    
    * Update setSessionStatus
    
    * Update getSessionStatus
    
    * Update selectCalibrationPage
    
    * Formatting
    
    * Fix intToByteArray
    
    * Implement extendBytearray
    
    * Update setMemoryTransferAddress
    
    * Update upload
    
    * Remove extra variable definitions
    
    * Make argument keyword
    
    * Re-implement shortUpload
    
    * Update unlock

--- src/winnie/connection.py
+++ src/winnie/connection.py
@@ -5,6 +5,7 @@ from winnie import resourceMask as rm
 from winnie import formatting
 from winnie import verification
 from winnie import sessionStatus as sStatus
+from winnie import byteops
 
 class Connection:
 	def __init__(self, channel: canlib.Channel, id: int, debug: bool = False):
@@ -28,6 +29,17 @@ class Connection:
 		result = self.channel.read(timeout=500)
 		return result.data
 
+	def constructCRO(self, commandCode: int, payload: bytearray=None) -> bytearray:
+		if len(payload) > 6:
+			raise ValueError(f"Payload must be 6 bytes or less, was actually {len(payload)}")
+		if payload == None:
+			payload = bytearray()
+		cro = bytearray([commandCode, self.counter])
+		cro.extend(payload)
+		if len(cro) < 8:
+			cro.extend([0x00] * (8 - len(cro)))
+		return cro
+
 	def sendMessage(self, message: bytearray) -> bytearray:
 		if self.debug == True:
 			formatting.printByteArrayWithLabel("Message: ", message)
@@ -45,36 +57,36 @@ class Connection:
 		verification.verifyResponse(result, currentCounter)
 
 		return result, currentCounter
+	
+	def sendCRO(self, commandCode: int, payload: bytearray) -> bytearray:
+		message = self.constructCRO(commandCode, payload)
+		result, _ = self.sendMessage(message)
+		return result
 
 	def connect(self, stationID: int) -> bool:
 		if self.debug == True:
 			print("CONNECT")
-
-		splitID = listops.splitNumberByBytes(stationID, bigEndian=False)
-		message = bytearray([0x01, self.counter, splitID[0], splitID[1], 0, 0, 0, 0])
-		response, msgCounter = self.sendMessage(message)
+		idBytes = byteops.intToByteArray(stationID)
+		self.sendCRO(0x01, payload=idBytes)
 		self.connected = True
 		return True
 	
 	def disconnect(self, stationID: int, temporary=False) -> bool:
 		if self.debug == True:
 			print("DISCONNECT")
-
-		splitID = listops.splitNumberByBytes(stationID, bigEndian=False)
+		idBytes = byteops.intToByteArray(stationID)
 		temporaryByte = 0x01
 		if temporary == True:
 			temporaryByte = 0x00
-		message = bytearray([0x07, self.counter, temporaryByte, 0, splitID[0], splitID[1], 0, 0])
-		response, msgCounter = self.sendMessage(message)
+		payload = bytearray([temporaryByte, 0]) + idBytes
+		self.sendCRO(0x07, payload=payload)
 		self.connected = False
 		return True
 	
 	def exchangeID(self) -> Tuple[rm.ResourceMask, rm.ResourceMask]:
 		if self.debug == True:
 			print("EXCHANGE_ID")
-
-		message = bytearray([0x17, self.counter, 0, 0, 0, 0, 0, 0])
-		response, msgCounter = self.sendMessage(message)
+		response = self.sendCRO(0x17)
 		# Initialise two resource mask objects
 		availabilityMask = rm.ResourceMask(False, False, False)
 		protectionMask = rm.ResourceMask(False, False, False)
@@ -86,9 +98,8 @@ class Connection:
 	def getSeed(self, resourceMask: rm.ResourceMask) -> List[int]:
 		if self.debug == True:
 			print("GET_SEED")
-
-		message = bytearray([0x12, self.counter, resourceMask.getInteger(), 0, 0, 0, 0, 0])
-		response, msgCounter = self.sendMessage(message)
+		payload = bytearray([resourceMask.getInteger()])
+		response = self.sendCRO(0x12, payload=payload)
 		return response[4:]
 
 	def unlock(self, key: bytearray) -> rm.ResourceMask:
@@ -96,27 +107,19 @@ class Connection:
 			print("UNLOCK")
 		if len(key) != 6:
 			raise ValueError(f"Key must be 6 bytes long, was {len(key)} bytes long")
-		message = bytearray([0x13, self.counter])
-		message.extend(key)
-		response, msgCounter = self.sendMessage(message)
+		response = self.sendCRO(0x13, payload=key)
 		return rm.maskFromInt(response[3])
 
 	def setMemoryTransferAddress(self, mtaNumber: int, extension: int, address: int) -> bool:
 		if self.debug == True:
 			print("SET_MTA")
-
 		if mtaNumber != 0 and mtaNumber != 1:
 			raise ValueError("Memory transfer address number must be 0 or 1")
-		# Construct the message
-		message = [0x02, self.counter, mtaNumber, extension]
-		message.extend(listops.splitNumberByBytes(address, bigEndian=True))
-		# Add leading 0s to address if necessary (this is why we need to switch to byte arrays)
-		if len(message) < 8:
-			zerosToAdd = 8 - len(message)
-			message = message[:4] + [0] * zerosToAdd + message[4:]
-		# Send message and handle response
-		message = bytearray(message)
-		response, msgCounter = self.sendMessage(message)
+		addressBytes = byteops.intToByteArray(address, bigEndian=True)
+		# Add leading zeros to address
+		addressBytes = byteops.extendBytearray(addressBytes, 4, left=True)
+		payload = bytearray([mtaNumber, extension]) + addressBytes
+		self.sendCRO(0x02, payload=payload)
 		self.mta = address
 		self.mtaExtension = extension
 		self.mtaNumber = mtaNumber
@@ -125,29 +128,30 @@ class Connection:
 	def upload(self, blockSize: int) -> bytearray:
 		if self.debug == True:
 			print("UPLOAD")
-
 		if blockSize > 5:
 			raise ValueError("Block size must be 5 bytes or less")
-		message = bytearray([0x04, self.counter, blockSize, 0, 0, 0, 0, 0])
-		response, msgCounter = self.sendMessage(message)
+		payload = bytearray([blockSize])
+		response = self.sendCRO(0x04, payload=payload)
 		self.mta += blockSize
 		return response[3:3+blockSize]
 	
-	# def shortUpload(self, blockSize: int, extension: int, address: int) -> bytearray:
-	# 	if self.debug == True:
-	# 		print("SHORT_UP")
-	# 	if blockSize > 5:
-	# 		raise ValueError("Block size must be 5 bytes or less")
-	# 	message = bytearray([0x0F, self.counter, blockSize, extension, address, 0, 0, 0])
-	# 	response, msgCounter = self.sendMessage(message)
-	# 	return response[3:3+blockSize]
+	def shortUpload(self, blockSize: int, extension: int, address: int) -> bytearray:
+		if self.debug == True:
+			print("SHORT_UP")
+		if blockSize > 5:
+			raise ValueError("Block size must be 5 bytes or less")
+		addressBytes = byteops.intToByteArray(address, bigEndian=True)
+		# Add leading zeros to address
+		addressBytes = byteops.extendBytearray(addressBytes, 4, left=True)
+		payload = bytearray(blockSize, extension) + addressBytes
+		response = self.sendCRO(0x0F, payload=payload)
+		return response[3:3+blockSize]
 	
 	def getCCPVersion(self, mainVersion: int, release: int) -> Tuple[int, int]:
 		if self.debug == True:
 			print("GET_CCP_VERSION")
-
-		message = bytearray([0x1B, self.counter, mainVersion, release, 0, 0, 0, 0])
-		response, msgCounter = self.sendMessage(message)
+		payload = bytearray([mainVersion, release])
+		response = self.sendCRO(0x1B, payload=payload)
 		returnedMainVersion = int(response[3])
 		returnedRelease = int(response[4])
 		return returnedMainVersion, returnedRelease
@@ -155,14 +159,11 @@ class Connection:
 	def download(self, data: bytearray) -> bool:
 		if self.debug == True:
 			print("DOWNLOAD")
-
 		dataLength = len(data)
 		if dataLength > 5:
 			raise ValueError("Data must be 5 bytes or less")
-		message = bytearray([0x03, self.counter, dataLength])
-		message.extend(data)
-		message = listops.padToLength(message, 8, padding=0)
-		response, msgCounter = self.sendMessage(message)
+		payload = bytearray([dataLength]) + data
+		response = self.sendCRO(0x03, payload=payload)
 		self.mtaExtension = int(response[3])
 		self.mta = listops.listToInt(list(response[4:8]))
 		return True
@@ -173,8 +174,7 @@ class Connection:
 		dataLength = len(data)
 		if dataLength != 6:
 			raise ValueError("Data must be 6 bytes long")
-		message = bytearray([0x23, self.counter, data[0], data[1], data[2], data[3], data[4], data[5]])
-		response, msgCounter = self.sendMessage(message)
+		response = self.sendCRO(0x23, payload=data)
 		self.mtaExtension = int(response[3])
 		self.mta = listops.listToInt(list(response[4:8]))
 		return True
@@ -182,23 +182,18 @@ class Connection:
 	def setSessionStatus(self, status: sStatus.sessionStatus) -> bool:
 		if self.debug == True:
 			print("SET_S_STATUS")
-		
 		sessionInt = status.getInteger()
-		
-		message = bytearray([0x0C, self.counter, sessionInt, 0, 0, 0, 0, 0])
-		response, msgCounter = self.sendMessage(message)
+		self.sendCRO(0x0C, payload=bytearray([sessionInt]))
 		return True
 
 	def getSessionStatus(self) -> sStatus.sessionStatus:
 		if self.debug == True:
 			print("GET_S_STATUS")
-		message = bytearray([0x0D, self.counter, 0, 0, 0, 0, 0, 0])
-		response, msgCounter = self.sendMessage(message)
+		response = self.sendCRO(0x0D)
 		return sStatus.statusFromInt(response[3])
 
 	def selectCalibrationPage(self) -> bool:
 		if self.debug == True:
 			print("SELECT_CAL_PAGE")
-		message = bytearray([0x11, self.counter, 0, 0, 0, 0, 0, 0])
-		response, msgCounter = self.sendMessage(message)
+		self.sendCRO(0x11)
 		return True
