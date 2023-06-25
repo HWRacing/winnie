ed90c02b83ded31bc701f29c0e935d939c053e33
Hamish Knox <hrjkknox@gmail.com>
2023-06-25 17:06:53 +0100

    Implement test_maskFromInt

--- tests/test_resourceMask.py
+++ tests/test_resourceMask.py
@@ -3,3 +3,7 @@ from winnie import resourceMask
 def test_getInteger():
 	rm = resourceMask.ResourceMask(True, True, True)
 	assert rm.getInteger() == 0b01000011
+
+def test_maskFromInt():
+	rm = resourceMask.maskFromInt(0b01000011)
+	assert rm.read == True and rm.write == True and rm.flash == True
