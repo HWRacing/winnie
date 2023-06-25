44621a481df50872c262a42c6062e52f763eafdd
Hamish Knox <hrjkknox@gmail.com>
2023-06-25 12:24:48 +0100

    Implement intToByteArray

---
+++ src/winnie/byteops.py
@@ -0,0 +1,9 @@
+def intToByteArray(n: int, bigEndian: bool = False) -> bytearray:
+	output = []
+	while num > 0:
+		output.append(num % 256)
+		# Shift the number 8 bits to the right
+		num //= 256
+	if bigEndian == True:
+		output.reverse()
+	return bytearray(output)
