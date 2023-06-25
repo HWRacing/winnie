6cbaaba0ab9c3534f0677ff5de3a818012c1b7d4
Hamish Knox <hrjkknox@gmail.com>
2023-06-25 17:57:21 +0100

    Actually add assert statements to tests

--- tests/test_sessionStatus.py
+++ tests/test_sessionStatus.py
@@ -2,24 +2,24 @@ from winnie import sessionStatus
 
 def test_getInteger_all():
 	ss = sessionStatus.sessionStatus(True, True, True, True, True)
-	expectedResult = 0b11000111
+	assert ss.getInteger() == 0b11000111
 
 def test_getInteger_cal():
 	ss = sessionStatus.sessionStatus(True, False, False, False, False)
-	expectedResult = 0b00000001
+	assert ss.getInteger() == 0b00000001
 
 def test_getInteger_daq():
 	ss = sessionStatus.sessionStatus(False, True, False, False, False)
-	expectedResult = 0b00000010
+	assert ss.getInteger() == 0b00000010
 
 def test_getInteger_resume():
 	ss = sessionStatus.sessionStatus(False, False, True, False, False)
-	expectedResult = 0b00000100
+	assert ss.getInteger() == 0b00000100
 
 def test_getInteger_store():
 	ss = sessionStatus.sessionStatus(False, False, False, True, False)
-	expectedResult = 0b01000000
+	assert ss.getInteger() == 0b01000000
 
 def test_getInteger_run():
 	ss = sessionStatus.sessionStatus(False, False, False, False, True)
-	expectedResult = 0b10000000
+	assert ss.getInteger() == 0b10000000
